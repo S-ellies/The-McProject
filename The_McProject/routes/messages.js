fa07bd2253bfa6d7b1197ed9b6178a8c14e79c9a
Ruairí Murphy <47370759+RuMurf@users.noreply.github.com>
2020-03-07 19:26:50 +0000

    added messages.js to handle message related APIs

---
+++ The_McProject/routes/messages.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const Message = require('../models/messageSchema');
+
+/* POST message to db */
+router.post('/addMessage', function(req, res, next) {
+    // Extract the request body which contains the messages
+    const message = new Message(req.body);
+    message.save(function (err, savedMessage) {
+
+        if (err)
+            throw err;
+
+        res.json({
+            "id": savedMessage._id
+        });
+    });
+});
+
+/* GET messages */
+router.get('/getMessages', function(req, res, next) {
+
+    Message.find({}, function (err, messages) {
+        if (err)
+            res.send(err);
+
+        res.json(messages);
+    });
+});
+
+/*GET conversation between 2 users*/
+router.get('/getConversation', function(req, res, next) {
+
+    Message.find({sender:req.query.sender, recipient:req.query.recipient}, function (err, messages) {
+        if (err)
+            res.send(err);
+
+        res.json(messages);
+
+    }).sort( { date_created: -1 } );
+});
+
+/* DELETE message */
+router.delete('/removeMessage/:id', function(req, res, next){
+
+    const id = req.params.id;
+    Message.deleteOne({_id:id}, function (err) {
+        if (err)
+            res.send(err);
+
+        res.json({status : "Successfully removed the message"});
+    });
+});
+
+module.exports = router;
