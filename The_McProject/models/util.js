11597285cbde252999c20ea8235288e06943a9dc
SEBASTIAN ELLIES(26-09-2019 13-18 <danu7_es@danu7>
2020-03-20 16:00:21 +0000

    duplicate code removed

--- The_McProject/models/util.js
+++ The_McProject/models/util.js
@@ -1,6 +1,4 @@
 var mongoose = require('mongoose');
 var connection = mongoose.connect('mongodb://mongodb5165es:pe2val@danu7.it.nuigalway.ie:8717/mongodb5165', {useUnifiedTopology: true, useNewUrlParser: true});
 exports.connection = connection;
-var mongoose = require('mongoose');
-var connection = mongoose.connect('mongodb://mongodb5165es:pe2val@danu7.it.nuigalway.ie:8717/mongodb5165');
-exports.connection = connection;
+
