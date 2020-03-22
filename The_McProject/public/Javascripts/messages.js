a34beaadf1500634be844c6dc2dfcf21b006d89e
Ruairí Murphy <47370759+RuMurf@users.noreply.github.com>
2020-03-22 05:14:21 +0000

    altered the getMessages function to only get messages between 2 people

--- The_McProject/public/Javascripts/messages.js
+++ The_McProject/public/Javascripts/messages.js
@@ -1,30 +1,32 @@
+var user = "Morf";
+var friend = "Boshco";
 $(document).ready(
     function() {
-        function getMessages() {
+        getMessages(user, friend);
+        function getMessages(sender, recipient) {
             $.ajax({
-                url: '/messages/getMessages',
+                url: '/messages/getConversation?sender='+sender+"&recipient="+recipient,
                 type: 'GET',
                 success: function (data) {
                     var messages = "";
                     for (var i = 0; i < data.length; i++) {
-                        messages += "<div class='row justify-content-md-center pt-4'>" +
-                            "<div class='card col-md-6'>"
-                            + data[i].message + "</div></div>";
+                        messages += "<div class='row justify-content-md-center pt-4'><div class='card col-md-6'><div class='row col-md-6'><h6>" + data[i].sender + "</h6></div>"+ data[i].message + "</div></div>";
                     }
                     $("#messages").html(messages);
                 }
             })
+            setTimeout(function() { getMessages(user, friend); }, 10000);
         }
 
         $('#send').click(function (event) {
             $.ajax({
                 url: '/messages/addMessage',
                 type: 'POST',
-                data: {sender: "Murf",
-                    recipient: "Jo Mama",
+                data: {sender: "Morf",
+                    recipient: "Boshco",
                     message: $('#messageText').val()},
                 success: function(result) {
-                    getMessages();
+                    getMessages(user, friend);
                 }
             });
         })
