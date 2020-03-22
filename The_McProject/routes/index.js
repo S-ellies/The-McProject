64a6a8dbe057fdb00ba69a78c32cd87c526e526f
Ruairí Murphy <47370759+RuMurf@users.noreply.github.com>
2020-03-22 01:41:06 +0000

    added crappy message page template and API to route to message page

--- The_McProject/routes/index.js
+++ The_McProject/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+/*GET messages page. */
+router.get('/messages', function(req, res, next) {
+    res.render('messages', {title: 'Messages'});
 });
 
 module.exports = router;
