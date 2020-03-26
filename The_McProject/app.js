d49dd7dceae6782cc90154032ec88553aa166afb
Ruairí Murphy <47370759+RuMurf@users.noreply.github.com>
2020-03-26 15:35:39 +0000

    made it a look a little better ;)

--- The_McProject/app.js
+++ The_McProject/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/images/The McLogo.png'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
