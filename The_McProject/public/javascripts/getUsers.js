65059cae9f2c5449417704c7a4d08532de0dbbf4
SEBASTIAN ELLIES(26-09-2019 13-18 <danu7_es@danu7>
2020-04-09 23:26:49 +0100

    Extended functionality of user search

--- The_McProject/public/javascripts/getUsers.js
+++ The_McProject/public/javascripts/getUsers.js
@@ -1,19 +1,10 @@
-/**
- * Retrieve user JSON
- * Stick it onto webpage
- * Repeat until good amount of users found
- * If time permits:
- *      1. Sort displayed users by:
- *          a. Distance(to a degree)
- *          b. Relevance(with some algorithm to determine relevance)
- *      2. Don't display already connected users
- */
+
 $(document).ready(
     function () {
         var user = $.cookie("Username"); //current user
         $("#searchForm").submit(function (event) {
             event.preventDefault();
-            
+
             $("#users").empty();
             var queryString = "/users/getUsers";
             var body = "";
@@ -24,8 +15,28 @@ $(document).ready(
                 type: 'GET',
                 success: function (data) {
                     body = "";
+                    //find index of current user
+                    var usrIndex;
                     for (var i = 0; i < data.length; i++) {
-                        //if (data[i].user_name !== user)
+                        if (data[i].user_name == user)
+                            usrIndex = i;
+                    }
+                    var friends = [];
+                    $.ajax({
+                        url: '/friends/getUserFriends',
+                        type: 'GET',
+                        success: function (data) {
+                            for(var i = 0; i < data.length -1; i++)
+                                friends.push(data[i]);
+                        }
+                    });
+                    //dislay all relevant users
+                    for (var i = 0; i < data.length; i++) {
+                        //skip friends
+                        if (friends.includes(i))
+                            console.log(i);
+                        //skip self
+                        else if (i !== usrIndex)
                             body += "<div id='" + data[i].user_name + "'>" + "<img src'" + data[i].image + "' alt='profile-pic'><h3>" + data[i].user_name + "</h3><h4>" + data[i].instrument + "</h4><p>" + data[i].bio + "</p></div>";
                     }
                     $("#users").append(body);
