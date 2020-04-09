0df499d40cdc35936ac3e59cf1b18cf98139f006
SEBASTIAN ELLIES(26-09-2019 13-18 <danu7_es@danu7>
2020-04-10 00:54:23 +0100

    Bug fixes in user search

--- The_McProject/public/javascripts/getUsers.js
+++ The_McProject/public/javascripts/getUsers.js
@@ -2,6 +2,7 @@
 $(document).ready(
     function () {
         var user = $.cookie("Username"); //current user
+        console.log(test);
         $("#searchForm").submit(function (event) {
             event.preventDefault();
 
@@ -15,26 +16,18 @@ $(document).ready(
                 type: 'GET',
                 success: function (data) {
                     body = "";
+                    var friends = [];
                     //find index of current user
                     var usrIndex;
                     for (var i = 0; i < data.length; i++) {
                         if (data[i].user_name == user)
                             usrIndex = i;
                     }
-                    var friends = [];
-                    $.ajax({
-                        url: '/friends/getUserFriends',
-                        type: 'GET',
-                        success: function (data) {
-                            for(var i = 0; i < data.length -1; i++)
-                                friends.push(data[i]);
-                        }
-                    });
+                    console.log(user);
                     //dislay all relevant users
                     for (var i = 0; i < data.length; i++) {
                         //skip friends
-                        if (friends.includes(i))
-                            console.log(i);
+                        if (data[usrIndex].friends.includes(data[i]._id));
                         //skip self
                         else if (i !== usrIndex)
                             body += "<div id='" + data[i].user_name + "'>" + "<img src'" + data[i].image + "' alt='profile-pic'><h3>" + data[i].user_name + "</h3><h4>" + data[i].instrument + "</h4><p>" + data[i].bio + "</p></div>";
